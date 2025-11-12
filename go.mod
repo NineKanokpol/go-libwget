665b33a6c211fbcf82776f13e0d4aca3882118d4
Nine <nineklp0869@gmail.com>
2025-11-12 10:44:47 +0700

    first release

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/NineKanokpol/go-libwget
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var version = "dev" // can be overridden by -ldflags
+
+func main() {
+	showVersion := flag.Bool("version", false, "print version and exit")
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Println(version)
+		return
+	}
+
+	fmt.Println("Hello world this is My Lib")
+}
