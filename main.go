209a567290ad4e5e8c99cbfef35325ef3180eb39
Nine <nineklp0869@gmail.com>
2025-11-12 11:27:55 +0700

    release v0.1.3

--- main.go
+++ main.go
@@ -1,20 +1,48 @@
+// /Users/nineimacm2/Documents/test-go-wget/main.go
 package main
 
 import (
 	"flag"
 	"fmt"
+	"os"
+	"os/exec"
 )
 
-var version = "dev" // can be overridden by -ldflags
+var version = "dev"
 
 func main() {
 	showVersion := flag.Bool("version", false, "print version and exit")
 	flag.Parse()
 
+	// mycli -version
 	if *showVersion {
 		fmt.Println(version)
 		return
 	}
 
-	fmt.Println("Hello world this is My LibVersion0.1.2Test")
+	// ถ้าไม่มีคำสั่งตามหลัง -> โชว์ข้อความเดิม
+	args := flag.Args()
+	if len(args) == 0 {
+		fmt.Println("Hello world this is My Lib")
+		return
+	}
+
+	// mycli pwd         -> รัน /usr/bin/pwd (ตาม PATH)
+	// mycli ls -la      -> รัน ls -la
+	cmdName := args[0]
+	cmdArgs := args[1:]
+
+	cmd := exec.Command(cmdName, cmdArgs...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		// ส่งต่อ exit code ที่แท้จริง (ถ้ามี) เพื่อให้สคริปต์เช็คสถานะได้
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			os.Exit(exitErr.ExitCode())
+		}
+		fmt.Fprintln(os.Stderr, "run error:", err)
+		os.Exit(1)
+	}
 }
